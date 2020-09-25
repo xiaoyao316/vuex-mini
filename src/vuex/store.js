57bd2180f48e2c92852e6c1856190c8c23b3f3a8
xiaoyao <xiaoy@dachentech.com.cn>
2020-09-25 09:29:58 +0800

    注释

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -54,6 +54,8 @@ function resetStoreVM (store, state) {
     })
   })
 
+  // installModule中已经将子模块的state全部定义到父模块上，这里创建vue实例后，便可代理所有state属性，
+  // 再配合Store类上对state属性定义的get代理方法，即$store.state代理到$store.vm._data.state，于是$store.state.name/$store.state.news.list等等都能正常取值
   store._vm = new Vue({
     data: {
       $$state: state
@@ -62,6 +64,7 @@ function resetStoreVM (store, state) {
   })
 }
 
+// 依靠path进行模块的递归安装
 function installModule (store, rootState, path, module) {
   const isRoot = !path.length
   const namespace = getNamespace(path)
@@ -69,48 +72,50 @@ function installModule (store, rootState, path, module) {
   if (!isRoot) {
     const parentState = getNestedState(rootState, path.slice(0, -1))
     const moduleName = path[path.length - 1]
+    // 除根模块，都向上级模块添加state属性
     Vue.set(parentState, moduleName, module.state)
   }
 
   const local = module.context = makeLocalContext(store, namespace, path)
 
+  // 往Store._actions中缓存被我们加了劫持逻辑的方法
   forEachValue(module.actions || {}, (handler, type) => {
     const namespacedType = namespace + type
     store._actions[namespacedType] = function wrappedMutationHandler (payload) {
+      // 想想写action方法时的参数，第一个参数结构一个对象，包含dispatch、commit方法，state数据这些，然后是payload
       handler.call(store, {
         dispatch: local.dispatch,
         commit: local.commit,
-        getters: local.getters,
-        state: local.state,
-        rootGetters: store.getters,
-        rootState: store.state
+        state: local.state
       }, payload)
     }
   })
 
+  // 往Store._mutations中缓存被我们加了劫持逻辑的方法
   forEachValue(module.mutations || {}, (handler, type) => {
     const namespacedType = namespace + type
     store._mutations[namespacedType] = function wrappedMutationHandler (payload) {
+      // 写mutation方法时的参数，第一个是模块对应的state, 第二个是payload
       handler.call(store, local.state, payload)
     }
   })
 
+  // 往Store._wrappedGetters中缓存被我们加了劫持逻辑的方法
   forEachValue(module.getters || {}, (handler, type) => {
     const namespacedType = namespace + type
     store._wrappedGetters[namespacedType] = function wrappedGetter (store) {
-      return handler(
-        local.state,
-        local.getters,
-        store.state,
-        store.getters
-      )
+      // 写getter时的参数，模块对应的state
+      return handler(local.state)
     }
   })
 
+  // 加劫持逻辑的唯一目的，就是为了传递参数，而且其中包含了对应模块的数据
+  // 一个模块注册完，收集一下
   if (namespace) {
     store._modulesNamespaceMap[namespace] = module
   }
 
+  // 继续递归向下处理模块
   forEachValue(module.modules || {}, (child, key) => {
     installModule(store, rootState, path.concat(key), child)
   })
